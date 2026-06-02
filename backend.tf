4ce610b8a116efe21a54a95bd86ed3a0cf350bd2
Yogi4157 <dyogesh4157@gmail.com>
2026-06-02 16:13:22 +0530

    code

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "yogesh-terraform-state-bucket"
+    key            = "terraform-project/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "terraform-locks"
+  }
+}
